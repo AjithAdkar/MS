f882d3f2b8e1ff9373df1a28ace5cf7d2f73450d
Ajith Kumar A G <Ajithkumara@sti.com>
2023-12-19 20:38:59 +0530

    frist commit

---
+++ discovery/src/test/java/com/sun/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
